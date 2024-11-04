2ea28bbb677717bff1e41bc117072c4b75d6fe67
Volodymyr Pelykh <volodymyrp8283@gmail.com>
2024-11-04 20:48:20 +0200

    feat: supabase init

---
+++ app/notes/page.tsx
@@ -0,0 +1,8 @@
+import { createClient } from '@/utils/supabase/server';
+
+export default async function Notes() {
+  const supabase = await createClient();
+  const { data: products, error } = await supabase.from("category").select();
+  console.log(products)
+  return <pre>{JSON.stringify(products, null, 2)}</pre>
+}
